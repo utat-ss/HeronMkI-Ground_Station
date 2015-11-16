95641883d419461bd27a9d87476a659eae109b7a
keenan-burnett <keenburn2004@gmail.com>
2015-11-16 16:56:18 -0500

    I created GroundPacketRouter.py which shall house the main packet router for the ground station software, this class will also be responsible for initialiing everything and starting the subsidiary services. I also created PUSService.py which is a subclass of Process from the multiprocessing library. I intend to create three more classes which are subclasses of PUSService. Then in GPR (GroundPacketRouter) I will only need to create instances of these classes and execute their start() method, which begins whatever code is located in an overritable method run().

---
+++ GroundPacketRouter.py
@@ -0,0 +1,173 @@
+"""
+FILE_NAME:			GroundPacketRouter.py
+
+AUTHOR:				Keenan Burnett
+
+PURPOSE:			this program is meant to start all other ground station software and
+					act as the interface between subsidiary services and the CLI / transceiver.
+
+FILE REFERENCES: 	PUSService.py, 
+
+LIBRARIES USED:		os, datetime, multiprocessing
+
+ABNORMAL TERMINATION CONDITIONS, ERROR AND WARNING MESSAGES: None yet.
+
+ASSUMPTIONS, CONSTRAINTS, CONDITIONS: None.
+
+NOTES:
+					...
+REQUIREMENTS:
+	-Python 2.7
+	-Linux Operating system (Ubuntu 14 was used)
+	-This class should be called via a normal terminal in Linux
+	-ex: python GrounPacketRouter.py
+	-For the time being, we will have a serial connection to an Arduino Uno
+	which will allow us to connect to the transceiver remotely.
+	-We are using the CC1120 dev board as our "transceiver" for the groundstation
+
+DEVELOPMENT HISTORY:
+11/16/2015			Created.
+
+"""
+
+
+import os
+from PUSService import *
+from datetime import *
+from multiprocessing import Process, Lock
+
+class groundPacketRouter(Process):
+	"""
+	Author: Keenan Burnett
+	Acts as the main packet router for PUS packets to/from the groundstation
+	as well as the CLI.
+	Creates other processes which are used to manage PUS services.
+
+	"""
+
+@classmethod
+def start(self):
+	"""
+	@purpose: Represents the main program for the ground packet router.
+	"""	
+	self.initialize()
+
+	while(1):
+		# Check the transceiver for an incoming packet
+		# Update FIFOs
+		# Check FIFOs for a required action
+
+def initialize(self):
+
+	absTime = datetime.timedelta(0)	# Set the absolute time to zero.
+	currentTime = datetime.date()
+
+	"""Get the absolute time from the satellite and update ours."""
+
+	# Create all the required FIFOs
+	os.mkfifo("/fifos/hkToGPR.fifo")
+	self.hkToGPRFifo = open("/fifos/hkToGPR.fifo", "rb")
+	os.mkfifo("/fifos/GPRtohk.fifo")
+	self.GPRTohkFifo = open("/fifos/GPRtohk.fifo", "wb")
+	os.mkfifo("/fifos/memToGPR.fifo")
+	self.memToGPRFifo = open("/fifos/memToGPR.fifo", "rb")
+	os.mkfifo("/fifos/GPRtomem.fifo")
+	self.GPRTomemFifo = open("/fifos/GPRtomem.fifo", "wb")
+	os.mkfifo("/fifos/fdirToGPR.fifo")
+	self.fdirToGPRFifo = open("/fifos/fdirToGPR.fifo", "rb")
+	os.mkfifo("/fifos/GPRtofdir.fifo")
+	self.GPRTofdirFifo = open("/fifos/GPRtofdir.fifo", "wb")
+	# Create all the files required for logging
+	eventPath = "/events/eventLog.%s.%s" %currentTime.month, %currentTime.day
+	if os.path.exists(eventPath):
+		eventLog = open(eventPath, "r+")
+	else:
+		eventLog = open(eventPath, "w")
+	hkPath = "/hk_logs/hkLog.%s.%s" %currentTime.month, %currentTime.day
+	if os.path.exists(hkPath):
+		hkLog = open(hkPath, "r+")
+	else:
+		hkLog = open(hkPath, "w")
+
+	# Create all the required PUS Services
+	self.HKGroundService = PUSService("/fifos/hkToGPR.fifo", "/fifos/GPRtohk.fifo", eventPath, hkPath, absTime.day, absTime.minute, absTime.minute, absTime.second, self.hkService)
+	self.MemoryGroundService = PUSService("/fifos/memToGPR.fifo", "/fifos/GPRtomem.fifo", eventPath, hkPath, absTime.day, absTime.minute, absTime.minute, absTime.second, self.memService)
+	self.FDIRGround = PUSService("/fifos/fdirToGPR.fifo", "/fifos/GPRtofdir.fifo", eventPath, hkPath, absTime.day, absTime.minute, absTime.minute, absTime.second, self.fdirService)
+
+	# TO-DO: Create 3 new classes which are subclasses of PUS Service
+
+	# Then we simply have to create them here and execute their start() method.
+
+	# Create Mutex logs for communicating with logs and the CLI.
+
+
+	# ...
+	return
+
+
+@classmethod
+def stop(self):
+	# Close all the files which were opened
+	self.hkToGPRFifo.close()
+	self.GPRTohkFifo.close()
+	self.memToGPRFifo.close()
+	self.GPRTomemFifo.close()
+	self.fdirToGPRFifo.close()
+	self.GPRTofdirFifo.close()
+
+
+def __init__(self):
+	"""
+	@purpose: Initialization method for the Ground Packet Router Class
+	"""
+	# Global variables for each PUS Service Instance
+	self.processID 				= None
+	self.serviceType 			= None
+	self.currentCommand 		= []
+	# Definitions to clarify which services represent what
+	self.dataLength 			= 137			# Length of the data section of PUS packets
+	self.packetLength 			= 152			# Length (in bytes) of the entire PUS packet
+	self.tcVerifyService 		= 1
+	self.hkService 				= 3
+	self.eventReportService 	= 5
+	self.memService 			= 6
+	self.timeService			= 9
+	self.kService				= 69
+	# Definitions to clarify which service subtypes represent what
+	# HOUSEKEEPING
+	self.newHKDefinition 		= 1
+	self.clearHKDefinition 		= 3
+	self.enableParamReport		= 5
+	self.disableParamReport 	= 6
+	self.reportHKDefinitions	= 9
+	self.hkDefinitionReport		= 10
+	self.hkReport 				= 25
+	# TIME
+	self.updateReportFreq		= 1
+	self.timeReport				= 2
+	# MEMORY
+	self.memoryLoadABS			= 2
+	self.dumpRequestABS			= 5
+	self.memoryDumpABS			= 6
+	self.checkMemRequest		= 9
+	self.memoryCheckABS			= 10
+	#K-SERVICE
+	self.addSchedule			= 1
+	self.clearSchedule			= 2
+	self.schedReportRequest		= 3
+	self.schedReport 			= 4
+	# Event Report ID
+	self.kickComFromSchedule	= 1
+	self.bitFlipDetected		= 2
+	self.memoryWashFinished		= 3
+	# Global Variables for Time
+	self.abs_day				= day
+	self.abs_hour				= hour
+	self.abs_minute				= minute
+	self.abs_second				= second
+
+if __name__ == '__main__':
+	x = groundPacketRouter()
+	x.start()
+	x.stop()
+	
---
+++ PUSService.py
@@ -0,0 +1,110 @@
+"""
+FILE_NAME:			PUSService.py
+
+AUTHOR:				Keenan Burnett
+
+PURPOSE:			This class shall house all the common methods and atributes of PUS services.
+
+FILE REFERENCES:
+
+LIBRARIES USED:		os, multiprocessing
+
+ABNORMAL TERMINATION CONDITIONS, ERROR AND WARNING MESSAGES: None yet.
+
+ASSUMPTIONS, CONSTRAINTS, CONDITIONS: None.
+
+NOTES:
+					...
+REQUIREMENTS:
+
+DEVELOPMENT HISTORY:
+11/16/2015			Created.
+
+"""
+
+import os
+from multiprocessing import Process, Lock
+
+class PUSService(Process):
+	"""
+	Superclass for all PUS services so that we can easily have some common methods and attributes.
+	"""
+@classmethod
+def clearCurrentCommand(self):
+	i = 0
+	for i = 0 to (self.dataLength + 10):
+		self.currentCommand[i] = 0
+	return
+
+@classmethod
+def logEventReport(severity, reportID, param1, param0, hour, minute):
+	"""
+	@purpose: This method writes a event report to the event log.
+	@param: severity: 1 = Normal, 2-4 = different levels of failure
+	@param: reportID: Unique to the event report, ex: self.bitFlipDetected
+	@param: param1,0: extra information sent from the satellite.
+	"""
+	# Event logs include time, which may have come from the satellite.
+
+
+def __init__(self, path1, path2, eventPath, hkPath, day, hour, minute, second, type):
+	"""
+	@purpose: Initialization method for the PUS service class.
+	@param: path1: path to the file being used as a one-way fifo TO this PUS Service Instance
+	@param: path2: path to the file being used as a one-way fifo FROM this PUS service Instance
+	@param: day, hour, minute, second: Time to be set & subsequently updated by the Ground Packet Router
+	"""
+	# Global variables for each PUS Service Instance
+	self.processID 				= None
+	self.serviceType 			= None
+	self.currentCommand 		= []
+	# FIFOs Required for communication witht the Ground Packet Router:
+	self.fifoToGPR				= open(path1, "wb")
+	self.fifoFromGPR			= open(path1, "rb")
+	# Definitions to clarify which services represent what
+	self.dataLength 			= 137			# Length of the data section of PUS packets
+	self.packetLength 			= 152			# Length (in bytes) of the entire PUS packet
+	self.tcVerifyService 		= 1
+	self.hkService 				= 3
+	self.eventReportService 	= 5
+	self.timeService			= 9
+	self.kService				= 69
+	# Definitions to clarify which service subtypes represent what
+	# HOUSEKEEPING
+	self.newHKDefinition 		= 1
+	self.clearHKDefinition 		= 3
+	self.enableParamReport		= 5
+	self.disableParamReport 	= 6
+	self.reportHKDefinitions	= 9
+	self.hkDefinitionReport		= 10
+	self.hkReport 				= 25
+	# TIME
+	self.updateReportFreq		= 1
+	self.timeReport				= 2
+	# MEMORY
+	self.memoryLoadABS			= 2
+	self.dumpRequestABS			= 5
+	self.memoryDumpABS			= 6
+	self.checkMemRequest		= 9
+	self.memoryCheckABS			= 10
+	#K-SERVICE
+	self.addSchedule			= 1
+	self.clearSchedule			= 2
+	self.schedReportRequest		= 3
+	self.schedReport 			= 4
+	# Event Report ID
+	self.kickComFromSchedule	= 1
+	self.bitFlipDetected		= 2
+	self.memoryWashFinished		= 3
+	# Global Variables for Time
+	self.abs_day				= day
+	self.abs_hour				= hour
+	self.abs_minute				= minute
+	self.abs_second				= second
+	# Files to be used for logging and housekeeping
+	self.eventLog = open(eventPath, a+)
+	self.hkLog = open(hkPath, a+)
+
+if __name__ == '__main__':
+	return
+	
