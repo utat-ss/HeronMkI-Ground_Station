7d22230e3a60cceb8b7002dd6fd049a8016ff43b
Keenan Burnett <keenburn2004@gmail.com>
2015-12-03 23:20:54 -0500

    Modified addSchedule so that the user can input a filename for new commands.

--- Create_Directory_Structure_Unix.sh
+++ Create_Directory_Structure_Unix.sh
@@ -4,8 +4,12 @@ mkdir events
 mkdir fifos
 mkdir ground_errors
 mkdir housekeeping
+mkdir housekeeping/logs
 mkdir image_data
 mkdir memory
+mkdir memory/load
+mkdir memory/dumps
+mkdir memory/checks
 mkdir payload_data
 mkdir schedule
 mkdir telemetry
